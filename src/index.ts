9957541bd0abfe2d89cbdc2765d2ccf2f5e03562
erikoc <32451595+erikoc@users.noreply.github.com>
2018-11-19 21:18:29 +0100

    Upgrade typescript,jest,ts-jest and types (#11)
    * Add .nvmrc file
    * Add files in dist/ to .gitignore except for countries

--- src/index.ts
+++ src/index.ts
@@ -63,14 +63,18 @@ const loadScript = (src: string, id: string) => {
   return new Promise((resolve, reject) => {
     const existingScript = document.getElementById(id)
     if (!existingScript) {
-      let s
-      s = document.createElement('script')
-      s.src = src
-      s.id = id
-      s.onload = resolve
-      s.onerror = reject
-      s.async = true
-      document.head.appendChild(s)
+      if (document.head) {
+        let s
+        s = document.createElement('script')
+        s.src = src
+        s.id = id
+        s.onload = resolve
+        s.onerror = reject
+        s.async = true
+        document.head.appendChild(s)
+      } else {
+        reject()
+      }
     } else {
       resolve()
     }
